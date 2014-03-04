434c405f801da34a7a635f9b0286cbce51016d68
Rob Prouse <rob@prouse.org>
2014-03-04 08:04:25 -0500

    Serialization of the configuration file

--- ProjectSync/Program.cs
+++ ProjectSync/Program.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using alteridem.net.Extensions;
+using Alteridem.ProjectSync.Model;
 
-namespace ProjectSync
+namespace Alteridem.ProjectSync
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var solution = new Solution(@"C:\src\NUnit\nunit-framework\NUnitFramework.sln");
+
+            var projectgroup = new ProjectGroup("nunit.framework");
+            solution.ProjectGroups.Add(projectgroup);
+
+            var project = new Project("nunit-framework-2.0");
+            project.Ignore.Add("async.cs");
+            project.Ignore.Add("lambda.cs");
+
+            projectgroup.Projects.Add(project);
+            projectgroup.Projects.Add(new Project("nunit-framework-3.5"));
+            projectgroup.Projects.Add(new Project("nunit-framework-4.0"));
+            projectgroup.Projects.Add(new Project("nunit-framework-4.5"));
+
+            solution.SerializeToFile("nunit.xml");
         }
     }
 }
